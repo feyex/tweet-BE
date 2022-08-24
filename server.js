88552277d68abce2b1c25f13189fca7c92ec93e0
feyisewa <feyisewaawotedu@gmail.com>
2022-08-24 20:12:15 +0200

    updated port no

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use('/api', yorubaRoute);
 app.use('/v1', userRoute);
 
 // start server
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 const server = app.listen(port, function(){
  console.log('Listening on port ' + port);
